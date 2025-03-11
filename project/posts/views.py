e65b06ff9ac09bd08ed7ac7d630eb2ae6e305ed6
mandari1ne <marina.stakhovets@gmail.com>
2025-03-11 19:07:58 +0300

    modified posts filters

--- project/posts/views.py
+++ project/posts/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
-from .models import Post, PostImage, Category
-
-# Create your views here.
+from .models import Post, Category
 
 def post_index(request):
     categories = Category.objects.all()
-    posts = Post.annotate_post_data().prefetch_related('images').order_by('-created_at')
+    category_id = request.GET.get('category')
+
+    posts = Post.annotate_post_data().prefetch_related('images')
+
+    if category_id:
+        posts = posts.filter(category_id=category_id)
+
+    posts = posts.order_by('-created_at')
 
     return render(request, 'posts_index.html', {
         'categories': categories,
