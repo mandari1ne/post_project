19fc371cef923a2fda17c446ad1d221a8be1e5ff
mandari1ne <marina.stakhovets@gmail.com>
2025-03-27 00:56:13 +0300

    add paginations

--- project/project/settings.py
+++ project/project/settings.py
@@ -112,6 +112,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
